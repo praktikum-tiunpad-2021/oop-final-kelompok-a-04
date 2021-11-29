928ff0b5afff06e9f843fce8d599dba385df2893
bagus2705 <bagusprasetyo2705@gmail.com>
2021-11-29 20:19:30 +0700

    sudoku

---
+++ src/Board.java
@@ -0,0 +1,39 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+public class Board extends JFrame {
+
+public  int jumlahKotak = 9;
+public Grid [] kotak = new Grid[jumlahKotak];
+
+public  Board(){
+
+    super("Sudoku");
+    setSize(700,700);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    JPanel panel = new JPanel(new GridLayout(3,3));
+    for(int i=0; i<jumlahKotak; i++){
+        kotak[i] = new Grid();
+        panel.add(kotak[i]);
+    }
+    add(panel, BorderLayout.CENTER);
+    setVisible(true);
+    setLocationRelativeTo(null);
+}
+
+public class exitaction implements ActionListener{
+    public void actionPerformed (ActionEvent e){
+        System.exit(0);
+    }
+}
+
+public static void main(String[] args) { 
+    Board board = new Board();
+}    
+}
